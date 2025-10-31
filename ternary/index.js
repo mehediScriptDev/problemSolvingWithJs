bd76e9dbae17d1fa9ca511126e997171986fcafc
mehediScriptDev <mehedi25122003@gmail.com>
2025-10-31 19:03:59 +0600

    ternary recap

---
+++ ternary/index.js
@@ -0,0 +1,9 @@
+const myNumber = 33;
+
+if(myNumber%11 === 0){
+    console.log("This is a truthy statement");
+} else {
+    console.log("This is a falsy statement");
+}
+
+myNumber % 11 === 0 ? console.log("thruthy") : console.log("falsy");
